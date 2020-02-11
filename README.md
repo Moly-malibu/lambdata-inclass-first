a23478c1eb4cddfe0e3e073b21c3152cf6d0db67
Moly-malibu <lilianalambda@gmail.com>
2020-02-11 12:41:54 -0800

    Update readme

---
+++ README.md
@@ -0,0 +1,13 @@
+# My Lambdata Package
+
+# Installation
+
+Installation Instructions:
+
+TODO
+
+# Usage
+
+usage Instructions:
+
+TODO
--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r") as fh:
 
 setup(
     name="Malibu-lambda-1",
-    version="1.2",
+    version="0.1",
     author="Monica Bustamante",
     author_email="mbustamante@monicadatascience.com",
-    description="Incomedata",
+    description="Incomedata package for lambda school Ds Unit 3",
     long_description=long_description,
     long_description_content_type="text/markdown", # required if using a md file for long desc
     license="MIT",
-    url="https://github.com/s2t2/game-utils-py",
+    url="https://github.com/Moly-malibu/lambdata-inclass-first",
     keywords="Income",
-    packages=find_packages() # ["game_utils"]
+    packages=find_packages() # ["my_lambdate"]
 )
