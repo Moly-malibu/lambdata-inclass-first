e4a6ddb5235becf286be715dffe3c9e574f54b7c
Moly-malibu <lilianalambda@gmail.com>
2020-02-11 08:32:30 -0800

    Update repo urg

--- my_lambdata.py/my_mod.py
+++ my_lambdata.py/my_mod.py
@@ -1,3 +1,22 @@
-def enlarge(n):
-    return n*100
-    
+from sklearn.metrics import plot_confusion_matrix
+
+#Dataframe nulls
+
+df.isnull()
+
+#print report
+
+import pandas_profiling
+df.profile_reporte()
+
+#Report Confusion Matrix:
+
+# Check scikit-learn version
+import sklearn
+sklearn.__version__
+
+y_true = [1, 0, 1, 2, 0, 1]
+y_pred = [0, 1, 2, 1, 0, 0]
+confusion_matrix(y_true, y_pred)
+
+plot_confusion_matrix(pipeline, y_true, y_pred, values_format='.0f', xticks_rotation='vertical');
